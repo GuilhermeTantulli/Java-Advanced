a7cddb07f520d50a5865c536870615640ebc5821
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-05 22:42:29 -0300

    Updated: Criados os métodos Post/Delete/Put

--- src/main/java/br/com/fiap/financaspro/model/Categoria.java
+++ src/main/java/br/com/fiap/financaspro/model/Categoria.java
@@ -3,8 +3,10 @@ package br.com.fiap.financaspro.model;
 import java.util.Random;
 
 public record Categoria (Long id, String nome, String icone) {
+
     public Categoria(Long id, String nome, String icone) {
-        this.id = Math.abs(new Random().nextLong());
+        var key = (id == null)? Math.abs( new Random().nextLong() ) : id;
+        this.id = key;
         this.nome = nome;
         this.icone = icone;
     }
