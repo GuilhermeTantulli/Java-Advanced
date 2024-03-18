fd9388176f1539e7f575a60c2658448272ff1d09
Guilherme Tantulli <guilherme.tantulli@gmail.com>
2024-03-18 19:20:40 -0300

    Updated: Refatoração da API Rest

--- src/main/java/br/com/fiap/financaspro/controller/CategoriaController.java
+++ src/main/java/br/com/fiap/financaspro/controller/CategoriaController.java
@@ -1,5 +1,8 @@
 package br.com.fiap.financaspro.controller;
 
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
+
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -37,11 +40,10 @@ public class CategoriaController {
     }
 
     @PostMapping
-    @ResponseStatus(code = HttpStatus.CREATED)
-    public Categoria cadastrarCategoria(@RequestBody Categoria categoria) {
+    @ResponseStatus(CREATED)
+    public Categoria create(@RequestBody Categoria categoria) {
         log.info("Cadastrando categoria: {}", categoria);
-        repository.save(categoria);
-        return categoria;
+        return repository.save(categoria);
     }
     
     @GetMapping("/{id}")
@@ -55,24 +57,21 @@ public class CategoriaController {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Object> destroy(@PathVariable Long id) {
+    @ResponseStatus(NO_CONTENT)
+    public void destroy(@PathVariable Long id) {
         log.info("apagando categoria");
 
         verificarSeExisteCategoria(id);
-
         repository.deleteById(id);
-        return ResponseEntity.noContent().build();
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Categoria> update(@PathVariable Long id, @RequestBody Categoria categoria) {
+    public Categoria update(@PathVariable Long id, @RequestBody Categoria categoria) {
         log.info("atualizando categoria com id {} para {}", id, categoria);
 
         verificarSeExisteCategoria(id);
-
         categoria.setId(id);
-        repository.save(categoria);
-        return ResponseEntity.ok(categoria);
+        return repository.save(categoria);
     }
 
     private void verificarSeExisteCategoria(Long id) {
